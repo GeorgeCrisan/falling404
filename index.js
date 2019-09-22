02420de3c7afe5c2cababeb43b7b5a8201197604
GeorgeCrisan <georgerdp@gmail.com>
2019-09-22 13:24:46 +0100

    change style

--- index.js
+++ index.js
@@ -1,22 +1,20 @@
 window.addEventListener("load", function () {
 
-    let nodes = "404e";
-    nodes = nodes.split("").filter((el) => {
-        return el !== " ";
-    });
-
+    let nodes = "404e404e404e404e404e404e404e404e";
+    nodes = nodes.split("");
     let nodestoappend = document.querySelectorAll(".letters");
 
     nodes.forEach((e, i) => {
         let node = document.createElement("span");
         node.style.left = (Math.random() * 75 + 'vw');
         node.style.animationDelay = i + "s";
-        node.innerText = e;
+        node.innerText = '404';
 
-        if (i === 3) {
+        if (i == 3 || i == 7 || i == 11) {
+            console.log('e', i);
             node.style.left = (50 + 'vw');
             node.style.animationDelay = i + "s";
-            node.innerText = 'error';
+            node.innerText = 'E r r o r';
         }
 
         nodestoappend[0].appendChild(node);
